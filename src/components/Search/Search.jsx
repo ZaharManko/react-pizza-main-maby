8ed6588be912ecc4f348c649d135139f0fb4da1e
ZaharManko <zaharmanko2008@gmail.com>
2024-01-26 19:56:48 +0200

    first commit

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,52 @@
+import stylle from './Search.module.scss';
+import { useCallback, useContext, useEffect, useRef, useState } from 'react';
+import { searchContext } from '../../App';
+import debounce from 'debounce';
+const Search = (props) => {
+    const [value, setValue] = useState('')
+    const { searchValue, setSearchValue } = useContext(searchContext);
+    const searchInputRef = useRef();
+    const testDebouns = useCallback(
+
+        debounce((str) => {
+            //console.log('pased 1000 ms')
+            setSearchValue(str)
+        }, 1000),
+        []
+
+
+    )
+
+
+
+    const inputonChange = (event) => {
+
+        setValue(event.target.value)
+        testDebouns(event.target.value)
+    }
+    const focusInput = () => {
+        setSearchValue('')
+        setValue('')
+        searchInputRef.current.focus()
+
+    };
+
+    return (
+        <div>
+            <input
+                onChange={inputonChange}
+                ref={searchInputRef}
+                value={value}
+                placeholder='search...'
+                className={stylle.search}
+
+            />
+            {value !== '' ?
+                <img onClick={focusInput} className={stylle.cross} />
+                : null
+            }
+        </div>
+    );
+}
+
+export default Search
